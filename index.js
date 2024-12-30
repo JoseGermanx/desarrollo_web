3efabb6adfce5fee6ebbece4d5ee171612231450
Jose Germán Martínez <jgermanmm@gmail.com>
2024-12-30 12:55:03 -0300

    Añadir estilos y scripts a contenedores.html, crear archivos style.css e index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+
+
+console.log("Hola mundo")
+
+
+function accion(){
+  document.getElementById("logo").innerHTML = "Hola mundo"  
+}
+
+function accion2(){
+  document.getElementById("logo").innerHTML = "Hola mundo 2"  
+}
+
+function accion3(){
+    alert("Estas a punto de eliminar tus datos")
+    // lógica para eliminar datos de una db
+    // ENVIAMOS  LA PETICIÓN PARA ELIMINAR -->>>
+    // <<<---- ESPERAMOS RESPUESTA
+
+    // SI LA RESPUESTA ES EXITOSA
+    console.log("Todo ok")
+
+    // SI NO ES SATISFACTORIA
+    console.error("Error")
+
+}
+
+function mensajeError(){
+    document.getElementById("msg-error").innerHTML = "Error al enviar el formulario"
+}
+
+document.getElementById("main").click
+
+console.log("Hola mundo 2")
+console.warn("Hola mundo 3")
+console.error("Hola mundo 4")
+
+
+
